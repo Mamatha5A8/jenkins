bb343734a31f7b140bb18b7afc9d9ca418860204
Mamatha <Mamatha@LAPTOP-7J9ULA4Q>
2022-11-13 21:56:51 +0530

    marven program by using jenkins

---
+++ src/main/java/csejenkins/jenkins5a8/App.java
@@ -0,0 +1,13 @@
+package csejenkins.jenkins5a8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
